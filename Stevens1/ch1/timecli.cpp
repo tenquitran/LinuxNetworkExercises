31a207a176d164b3c6587f5854711bf5110799e1
tenquitran <tenquitran@gmail.com>
2022-06-01 21:13:31 +0300

    Merged the common headers

--- Stevens1/ch1/timecli.cpp
+++ Stevens1/ch1/timecli.cpp
@@ -1,23 +1,28 @@
-#include "../common.h"
+#include "../../common.h"
 
 
 int main(int argc, char* argv[])
 {
 	if (argc < 3)
 	{
-		std::cerr << "Usage:\n\n" 
-		          << argv[0] << " <ip_address> <port>" << std::endl;
+		showUsageCli(argv[0]);
 		return 1;
 	}
 	
-	int port = std::stoi(argv[2]);
+	std::string ipAddress;
+	unsigned short port = {};
+	
+	if (!parseArgumentsCli(argc, argv, ipAddress, port))
+	{
+		return 2;
+	}
 	
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	
 	if (-1 == sock)
 	{
 		perror("socket()");
-		return 2;
+		return 3;
 	}
 	
 	sockaddr_in srvAddr;
@@ -26,16 +31,16 @@ int main(int argc, char* argv[])
 	srvAddr.sin_family = AF_INET;
 	srvAddr.sin_port   = htons(port);
 	
-	if (inet_pton(AF_INET, argv[1], &srvAddr.sin_addr) < 1)
+	if (inet_pton(AF_INET, ipAddress.c_str(), &srvAddr.sin_addr) < 1)
 	{
 		perror("inet_pton()");
-		return 3;
+		return 4;
 	}
 	
 	if (-1 == connect(sock, (const sockaddr *)&srvAddr, sizeof(srvAddr)))
 	{
 		perror("connect()");
-		return 4;
+		return 5;
 	}
 	
 	char buff[MAX_LINE + 1];
@@ -48,14 +53,14 @@ int main(int argc, char* argv[])
 		if (EOF == fputs(buff, stdout))
 		{
 			perror("fputs()");
-			return 5;
+			return 6;
 		}
 	}
 	
 	if (n < 0)
 	{
 		perror("read()");
-		return 6;
+		return 7;
 	}
 
 	return 0;
--- common.h
+++ common.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 #include <string>
+#include <iostream>
+#include <cstring>    // memset()
+#include <climits>
+
+
+#include <sys/socket.h>
+#include <netinet/in.h>    // sockaddr_in
+#include <arpa/inet.h>     // inet_pton()
+#include <unistd.h>        // read()
+
+
+// Maximum line length.
+const size_t MAX_LINE = 4096;
 
 ///////////////////////////////////////////////////////////////////////
 
